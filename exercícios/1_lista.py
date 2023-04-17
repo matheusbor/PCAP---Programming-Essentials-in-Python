a296abc7e06b0d1491bd826eeea077ae2fea96a6
Matheus Borges <matheuslb2003@gmail.com>
2023-04-16 23:20:02 -0300

    novos exercícios

---
+++ exercícios/1_lista.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5]
+
+print("Valores da lista: ", numbers)
+
+numbers[0] = 111
+
+print("Valores modificados: ", numbers)
+
+numbers[3-2] = numbers[4]
+print("Valores modificados 2: ", numbers)
---
+++ exercícios/básico_listas.py
@@ -0,0 +1,7 @@
+hat_list = [1, 2, 3, 4, 5]
+
+hat_list[2] = int(input("Digite um inteiro: "))
+
+del hat_list[-1]
+
+print("O comprimento é: ", hat_list)
