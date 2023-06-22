a064afd044a442bc4058076e83c770945e2a4ea0
Matheus Borges <matheuslb2003@gmail.com>
2023-06-22 12:24:07 -0300

    e isso ai

---
+++ exercícios/primeira_função.py
@@ -0,0 +1,14 @@
+def message():
+    print("Digite um inteiro positivo: ")
+    
+def sum():
+    soma = num1 + num2
+    print(soma)
+    
+message()
+num1 = int(input())
+
+message()
+num2 = int(input())
+
+sum()
