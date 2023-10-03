505213137e901c0967571b46b3f454643b5c543c
Matheus Borges <matheuslb2003@gmail.com>
2023-10-02 23:58:34 -0300

    add

--- exercicios_parte2/POO/pilha.py
+++ exercicios_parte2/POO/pilha.py
@@ -13,8 +13,36 @@ class stack:
 objeto = stack()
 objeto2 = stack()
 
-objeto.push(5) # nossa função se torna um método, lembrar de chamar como método
+class soma_pilha(stack):
+	def __init__(self):
+		stack.__init__(self)
+		self.__sum = 0
+	
+	def push(self, val):
+		self.__sum += val
+		stack.push(self, val) #to chamando o método da superclasse e deixa a implementação dela resolver
+	
+	def pop(self):
+		val = stack.pop(self) #não preciso chamar a função duas vezes porque excluiria duas vezes
+		self.__sum -= val
+		return val
+	def get_soma(self):
+		return self.__sum
+
+
+objeto3 = soma_pilha()
+
+for i in range(10):
+	objeto3.push(i)
+print("A soma é: ",objeto3.get_soma()) 
+
+for i in range(10):
+	objeto3.pop()
+print("Com a exclusão a soma é: ",objeto3.get_soma())
+
+
+''' objeto.push(5) # nossa função se torna um método, lembrar de chamar como método
 objeto2.push(objeto.pop())
 
 print(objeto2.pop())
-#print(len(objeto.stack_lista)) não é mais possível por ser privada
+#print(len(objeto.stack_lista)) não é mais possível por ser privada'''
