8546e286d789d7b3a9dd3107fc3c21c9cefc6432
Matheus Borges <matheuslb2003@gmail.com>
2023-11-01 15:42:43 -0300

    Update contador_horas.py

--- exercicios_parte2/POO/contador_horas.py
+++ exercicios_parte2/POO/contador_horas.py
@@ -1,30 +1,26 @@
 class Timer:
     def __init__(self, hour = 0, minute = 0, seconds = 0):
         self.__seconds = hour * 3600 + minute * 60 + seconds
-        self.__minutes
-        self.__hours
+        self.__minutes = 0
+        self.__hours = 0
     def __str__(self):
-        if self._Timer.__seconds >= 3600:
-            self._Timer.__hours = self._Timer.__seconds // 3600
-            self._Timer.__seconds -= self._Timer.__hours * 3600
-        elif self._Timer.__seconds >= 60:
-            self._Timer.__minutes = self._Timer.__seconds // 60
-            self._Timer.__seconds -= self._Timer.__minutes * 60
+        if self.__seconds >= 3600:
+            self.__hours = self.__seconds // 3600
+            self.__seconds -= self.__hours * 3600
+        elif self.__seconds >= 60:
+            self.__minutes = self.__seconds // 60
+            self.__seconds -= self.__minutes * 60
         
-        if self._Timer.__hours < 10:
-            self._Timer.__hours = "0" + str(self._Timer.__hours)
-        
-        return self._Timer.__hours + ":" + str(self._Timer.__minutes) + ":" + str(self._Timer.__seconds)
-
+        if self.__hours < 10:
+            return "0" + str(self.__hours) + ":" + str(self.__minutes) + ":" + str(self.__seconds)
+        return str(self.__hours) + ":" + str(self.__minutes) + ":" + str(self.__seconds)
     def next_second(self):
-        #
-        # Write code here
-        #
+        self.__seconds += 1
+        self.__str__
 
     def prev_second(self):
-        #
-        # Write code here
-        #
+        self.__seconds -= 1
+        self.__str__
 
 
 timer = Timer(23, 59, 59)
