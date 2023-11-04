e14998433823f62fd7a08c4a31a68f10c7ec45ee
Matheus Borges <matheuslb2003@gmail.com>
2023-11-04 18:50:05 -0300

    Create distancia_coordenadas.py

---
+++ exercicios_parte2/POO/distancia_coordenadas.py
@@ -0,0 +1,41 @@
+
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.__coordenada = [x, y]
+
+    def getx(self):
+        return self.__coordenada[0]
+
+    def gety(self):
+        return self.__coordenada[1]
+
+    def distance_from_xy(self, x, y):
+        #__intermediario = [x, self.__coordenada[1]]
+        __a = self.__coordenada[0] - x
+        __a = tratar_negativo(__a)
+        
+        __b = self.__coordenada[1] - y
+        __b = tratar_negativo(__b)
+        
+        return (__a*__a + __b * __b)**0.5
+        
+
+    def distance_from_point(self, point):
+        #__intermediario = [point.__coordenada[0], self.__coordenada[1]]
+        __a = self.__coordenada[0] - point.__coordenada[0]
+        __a = tratar_negativo(__a)
+        
+        __b = self.__coordenada[1] - point.__coordenada[1]
+        __b = tratar_negativo(__b)
+        
+        return (__a*__a + __b * __b)**0.5
+
+def tratar_negativo(x):
+    if x < 0:
+        return x * -1
+        
+point1 = Point(0, 0)
+point2 = Point(1, 1)
+print(point1.distance_from_point(point2))
+print(point2.distance_from_xy(2, 0))
