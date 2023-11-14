7ceed7b31a922c0c4a1df3ee2e65d959aab2e4c4
Matheus Borges <matheuslb2003@gmail.com>
2023-11-14 14:39:18 -0300

    update implementando função de ordem alfabética

--- exercicios_parte2/manipulação_arquivos/histograma.py
+++ exercicios_parte2/manipulação_arquivos/histograma.py
@@ -1,5 +1,35 @@
 from os import strerror
 
+
+def ordem_alfabetica(dicionario):
+    alfabeto = list(dicionario.keys())
+    
+    strg = "abcdefghijklmnopqrstuvxwyz"
+    strg2 = strg.upper()
+    posicao = []
+    i = 0
+    for letter in alfabeto:
+        if letter in strg or letter in strg2:
+            posicao.insert(i, strg.find(letter))
+            if posicao[i] == -1:
+                posicao[i] = strg2.find(letter)
+        i += 1
+    
+    lista = []
+    for i in range(len(alfabeto)):
+        minimo = min(posicao)
+        if posicao[i] == minimo:
+            lista.append(alfabeto[i])
+            posicao[i] = 99
+    return lista
+
+
+
+def printar_keys(lista, dicionario):
+    for letter in lista:
+        print(letter, "->",dicionario[letter])
+
+
 name = input("Digite o nome do arquivo: ")
 
 try:
@@ -15,16 +45,18 @@ try:
     
     dicionario = {}
     ler_character = lendo.read(1)
-    contadora = 0
     while ler_character != "":
         if ler_character not in dicionario.keys():
             dicionario[ler_character] = 0
-        contadora += 1
+        
         dicionario[ler_character] += 1
         ler_character = lendo.read(1)
 
 except IOError as e:
     print(strerror(e.errno))
     exit(e.errno)
+except BaseException as b:
+    print(strerror(b.errno))
+    exit(b.errno)
 finally:
-    print(dicionario)
+    printar_keys(ordem_alfabetica(dicionario), dicionario)
